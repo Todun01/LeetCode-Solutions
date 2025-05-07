ba67f2aa4c00cf3868144eabe76c76a4c85a0094
Oluwatodunni <obafemitodunni@gmail.com>
2025-05-07 15:43:59 +0100

    Update Longest Palindrome.py
    time complexity issue resolved

--- Longest Palindrome.py
+++ Longest Palindrome.py
@@ -1,19 +1,25 @@
 class Solution(object):
     def longestPalindrome(self, s):
-        indexCount = len(s)
-        beginningIndex = 0
-        longestPalindrome = ''
-        if len(s) == 1:
-            longestPalindrome = s
-        else:
-            while beginningIndex < len(s):
-                if s[beginningIndex:indexCount] == s[beginningIndex:indexCount][::-1]:
-                    if len(s[beginningIndex:indexCount]) > len(longestPalindrome):
-                        longestPalindrome = s[beginningIndex:indexCount]
-                indexCount -= 1
-                if indexCount == 0 :
-                    indexCount = len(s)
-                    beginningIndex += 1
-        return longestPalindrome
+        if len(s) <= 1:
+            return s
 
-        
+        def expand_around_center(left, right):
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                left -= 1
+                right += 1
+            return s[left + 1:right]  # valid palindrome
+
+        longest = ""
+        for i in range(len(s)):
+            # Odd-length palindrome
+            p1 = expand_around_center(i, i)
+            # Even-length palindrome
+            p2 = expand_around_center(i, i + 1)
+
+            # Choose the longer one
+            if len(p1) > len(longest):
+                longest = p1
+            if len(p2) > len(longest):
+                longest = p2
+
+        return longest
